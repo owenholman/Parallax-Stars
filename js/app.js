56694a4f0785939732e7405aa1271c153afda1db
Owen Holman <owenstholman@gmail.com>
2019-06-14 16:19:20 -0700

    Commenting

--- js/app.js
+++ js/app.js
@@ -1,54 +1,65 @@
+// canvas manipulation and declaration
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+// sets the canvas to be the browser's full dimensions
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// declarations for the html elements
 const gameBtn = document.getElementById('game-btn');
 const cover = document.getElementById('cover-content');
 const game = document.getElementById('game-content');
+
+// initialise variables that will be used/changed later
 var animation = null;
 var stars;
 var initGen = false;
 
-window.addEventListener("resize", function() {
+window.addEventListener("resize", e => { // begin resize event listener; triggers when browser is resized
+	// changes the canvas dimensions to properly reflect the changes
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-});
+}); // end resize event listener
 
-window.addEventListener("keydown", e => {
-	switch(e.code) {
+window.addEventListener("keydown", e => { // begin keydown event listener; triggers when the user presses a key
+	switch(e.code) { // begin switch conditional
 		case 'Enter':
-			gameBtn.click();
+			gameBtn.click(); // clicks html button if 'enter' key is pressed
 		break;
-	}
-});
+	} // end switch conditional
+}); // end event listener
 
-gameBtn.addEventListener('click', () => {
-	if (initGen == false) {
+gameBtn.addEventListener('click', () => { // begin click event listener attached to html button
+	if (initGen == false) { // runs if the initGen variable boolean is false
+		// deals with certain html elements accordingly
 		cover.style.display = 'none';
 		game.style.display = 'block';
-		stars = new StarObj();
-		play();
-		initGen = true;
-	} else if (initGen == true) {
-		window.cancelAnimationFrame(animation);
-		delete stars;
+
+		stars = new StarObj(); // sets the starObj object function to variable stars
+		play(); // runs play() function
+		initGen = true; // sets boolean to true
+	} else if (initGen == true) { // runs if the stars have already been generated
+		window.cancelAnimationFrame(animation); // cancels the animation (pauses)
+		delete stars; // deletes / recycles variable stars
+
+		// deals with the UI html elements accordingly
 		cover.style.display = 'flex';
 		game.style.display = 'none';
-		initGen = false;
-	}
-});
+		initGen = false; // sets the boolean generated to false
+	} // end of if/else
+}); // end of event listener of gameBtn
 
-const drawBackground = () => {
-	let gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+const drawBackground = () => { // start of drawBackground function
+	let gradient = ctx.createLinearGradient(0, 0, 0, canvas.height); // creates new variable with gradient
+	// adding color to a specific point on the gradient
 	gradient.addColorStop(0, "#0D0D0D");
 	gradient.addColorStop(0.9, "#031626");
 	gradient.addColorStop(1, "#032940");
-	return gradient;
-}
+	return gradient; // returning the gradient as a variable
+} // end of drawBackground function
 
-const play = () => {
+const play = () => { // start of play function
 	// recommence the loop
 	animation = window.requestAnimationFrame(play);
 
@@ -59,4 +70,4 @@ const play = () => {
 	// draw and update stars
 	stars.init();
 	stars.update();
-}
+} // end of play function
--- js/stars.js
+++ js/stars.js
@@ -1,100 +1,104 @@
-const StarObj = function() {
+const StarObj = function() { // start of starObj object function
+	// begin this. variables
 	this.initGen = false;
-
 	this.farStarArr = [];
 	this.maxFarStars = 150;
 	this.farStarLength;
-
 	this.closeStarArr = [];
 	this.maxCloseStars = 20;
 	this.closeStarLength;
 
+	// for each individual star
 	this.x;
 	this.y;
 	this.dyA = -0.8;
 	this.dyB = -0.4;
 
-	const randomNum = (min, max) => {
-		return Math.random() * (max - min) + min;
-	}
+	const randomNum = (min, max) => { // start randomNum function
+		return Math.random() * (max - min) + min; // returns a random number within the argued range
+	} // end function randomNum
 
-	this.init = () => {
-		if (this.initGen == false) {
+	this.init = () => { // start this.init function
+		if (this.initGen == false) { // if statement for when the boolean initGen is false
 			// for far stars
-			for (i = 0; i < this.maxFarStars; i++) {
+			for (i = 0; i < this.maxFarStars; i++) { // begin for loop
+				// for each individual star
 				this.x = randomNum(0, canvas.width);
 				this.y = randomNum(0, canvas.height);
 				this.farStarLength = randomNum(0, 3);
-				this.farStarArr.push({
+				this.farStarArr.push({ // begin pushing
 					x: this.x,
 					y: this.y,
 					dy: this.dyA,
 					length: this.farStarLength
-				});
-			}
+				}); // end pushing to array
+			} // end for loop
 
 			// for close stars
-			for (i = 0; i < this.maxCloseStars; i++) {
+			for (i = 0; i < this.maxCloseStars; i++) { // begin for loop
+				// for each individual star
 				this.x = randomNum(0, canvas.width);
 				this.y = randomNum(0, canvas.height);
 				this.closeStarLength = randomNum(4, 7);
-				this.closeStarArr.push({
+				this.closeStarArr.push({ // begin pushing to an array
 					x: this.x,
 					y: this.y,
 					dy: this.dyB,
 					length: this.closeStarLength
-				});
-			}
+				}); // end pushing to array
+			} // end for loop
 
 			// ensure this part of if is not generated again
 			this.initGen = true;
-		} else {
-			if (this.farStarArr.length < this.maxFarStars) {
-				for (i = 0; i < (this.maxFarStars - this.farStarArr.length); i++) {
-					this.x = randomNum(0, canvas.width);
-					this.y = canvas.height;
-					this.farStarLength = randomNum(0, 3);
-					this.farStarArr.push({
-						x: this.x,
-						y: this.y,
-						dy: this.dyA,
-						length: this.farStarLength
-					});
-				}
-				for (i = 0; i < (this.maxCloseStars - this.closeStarArr.length); i++) {
-					this.x = randomNum(0, canvas.width);
-					this.y = canvas.height;
-					this.closeStarLength = randomNum(4, 7);
-					this.closeStarArr.push({
-						x: this.x,
-						y: this.y,
-						dy: this.dyB,
-						length: this.closeStarLength
-					});
-				}
-			}
-		}
-	}
+		} else { // if the stars have already been generated
+			for (i = 0; i < (this.maxFarStars - this.farStarArr.length); i++) { // if there arent enough far stars filling up the max value we re-generate
+				// for each individual star
+				this.x = randomNum(0, canvas.width);
+				this.y = canvas.height;
+				this.farStarLength = randomNum(0, 3);
+				this.farStarArr.push({ // begin pushing to array
+					x: this.x,
+					y: this.y,
+					dy: this.dyA,
+					length: this.farStarLength
+				}); // end pushing to array
+			} // end for loop
+			for (i = 0; i < (this.maxCloseStars - this.closeStarArr.length); i++) { // if there arent enough close stars filling up the max value we re-generate
+				// for each individual star
+				this.x = randomNum(0, canvas.width);
+				this.y = canvas.height;
+				this.closeStarLength = randomNum(4, 7);
+				this.closeStarArr.push({ // begin pushing
+					x: this.x,
+					y: this.y,
+					dy: this.dyB,
+					length: this.closeStarLength
+				}); // end pushing to array
+			} // end for loop
+		} // end if/else conditional
+	} // end this.init function
 
-	this.update = () => {
-		for (i = 0; i < this.farStarArr.length; i++) {
-			if (this.farStarArr[i].y <= (0 - this.farStarArr[i].length)) {
-				this.farStarArr.splice(i, 1);
-			} else {
-				this.farStarArr[i].y += this.farStarArr[i].dy;
+	this.update = () => { // begin this.update function
+		for (i = 0; i < this.farStarArr.length; i++) { // begin for loop
+			if (this.farStarArr[i].y <= (0 - this.farStarArr[i].length)) { // checks if the y value of the object runs off-screen
+				this.farStarArr.splice(i, 1); // removes the object
+			} else { // runs if the object is still on display
+				this.farStarArr[i].y += this.farStarArr[i].dy; // accelerates the y value of each star
+				// draws each star
 				ctx.fillStyle = "#F2F2F2";
 				ctx.fillRect(this.farStarArr[i].x, this.farStarArr[i].y, this.farStarArr[i].length, this.farStarArr[i].length);
-			}
-		}
+			} // end of if/else conditional
+		} // end of for loop
 
-		for (i = 0; i < this.closeStarArr.length; i++) {
-			if (this.closeStarArr[i].y <= (0 - this.farStarArr[i].length)) {
-				this.closeStarArr.splice(i, 1);
-			} else {
-				this.closeStarArr[i].y += this.closeStarArr[i].dy;
+		for (i = 0; i < this.closeStarArr.length; i++) { // begin for loop
+			if (this.closeStarArr[i].y <= (0 - this.farStarArr[i].length)) { // checks if the y value of each star runs off-screen
+				this.closeStarArr.splice(i, 1); // removes the object from the array
+			} else { // runs if the object is still on the display
+				this.closeStarArr[i].y += this.closeStarArr[i].dy; // accelerates the y value of each star
+				// draws each star
 				ctx.fillStyle = "#F2F2F2";
 				ctx.fillRect(this.closeStarArr[i].x, this.closeStarArr[i].y, this.closeStarArr[i].length, this.closeStarArr[i].length);
-			}
-		}
-	}
-}
+			} // end of if/else conditional
+		} // end of for loop
+	} // end of this.update function
+} // end of starObj object function
